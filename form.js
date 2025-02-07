8f7dfec03738ee0dc78f115447c4ec4fbf7b62e6
DevanshMehra007 <devanshkmehra10@gmail.com>
2025-02-07 17:36:00 +0530

    update

--- form.js
+++ form.js
@@ -1,90 +1,103 @@
 
-let sub=()=>{
+// let sub=()=>{
 
-    let inpname=document.querySelector("#name").value;
-    let inpnum=document.querySelector("#number").value;
-    let inpmail=document.querySelector("#email").value;
-    let inppass=document.querySelector("#pass").value;
-    let inpcpass=document.querySelector("#Cpass").value;
+    // let inpname=document.querySelector("#name").value;
+    // let inpnum=document.querySelector("#number").value;
+    // let inpmail=document.querySelector("#email").value;
+    // let inppass=document.querySelector("#pass").value;
+    // let inpcpass=document.querySelector("#Cpass").value;
 
 
-    let errorname=document.querySelector("#errorname")
-    let errornum=document.querySelector("#errornum")
-    let errormail=document.querySelector("#errormail")
-    let errorpass=document.querySelector("#errorpass")
-    let errorcpass=document.querySelector("#errorcpass")
+    // let errorname=document.querySelector("#errorname")
+    // let errornum=document.querySelector("#errornum")
+    // let errormail=document.querySelector("#errormail")
+    // let errorpass=document.querySelector("#errorpass")
+    // let errorcpass=document.querySelector("#errorcpass")
 
-    if (inpname == "") {
-        let inpname=document.querySelector("#name")
-        inpname.placeholder="Please enter your name"
-        inpname.style.border="1px solid red"
-        errorname.innerHTML = "Please enter your name"
-        errorname.style.color = "red"
-        return false;
-    }
+    // if (inpname == "") {
+    //     let inpname=document.querySelector("#name")
+    //     inpname.placeholder="Please enter your name"
+    //     inpname.style.border="1px solid red"
+    //     errorname.innerHTML = "Please enter your name"
+    //     errorname.style.color = "red"
+    //     return false;
+    // }
 
-    if (inpnum == "") {
-        errornum.innerHTML = "*Please enter your number"
-        errornum.style.color = "red"
-        return false;
-    }
+    // if (inpnum == "") {
+    //     errornum.innerHTML = "*Please enter your number"
+    //     errornum.style.color = "red"
+    //     return false;
+    // }
 
-    if (inpmail == "") {
-        errormail.innerHTML = "*Please enter your email"
-        errormail.style.color = "red"
-        return false;
-    }
+    // if (inpmail == "") {
+    //     errormail.innerHTML = "*Please enter your email"
+    //     errormail.style.color = "red"
+    //     return false;
+    // }
 
-    if (inppass == "") {
-        errorpass.innerHTML = "*Please enter your password"
-        errorpass.style.color = "red"
-        return false;
-    }
+    // if (inppass == "") {
+    //     errorpass.innerHTML = "*Please enter your password"
+    //     errorpass.style.color = "red"
+    //     return false;
+    // }
 
-    if (inpcpass == "") {
-        errorcpass.innerHTML = "*Confirm password"
-        errorcpass.style.color = "red"
-        return false;
-    }
+    // if (inpcpass == "") {
+    //     errorcpass.innerHTML = "*Confirm password"
+    //     errorcpass.style.color = "red"
+    //     return false;
+    // }
 
-    if (inppass !== inpcpass) {
-        errorcpass.innerHTML = "*Confirm password is wrong"
-        errorcpass.style.color = "red"
-        document.querySelector("#Cpass").value=""
-        document.querySelector("#Cpass").focus()
-        return false;
+    // if (inppass !== inpcpass) {
+    //     errorcpass.innerHTML = "*Confirm password is wrong"
+    //     errorcpass.style.color = "red"
+    //     document.querySelector("#Cpass").value=""
+    //     document.querySelector("#Cpass").focus()
+    //     return false;
         
-    }
-    else if (isNaN(inpnum)) // If number than answer will be False
-    {
-        errornum.innerHTML = "*Please Enter Number Only"
-        errornum.style.color = "red"
-        return false;
-    } 
+    // }
+    // else if (isNaN(inpnum)) // If number than answer will be False
+    // {
+    //     errornum.innerHTML = "*Please Enter Number Only"
+    //     errornum.style.color = "red"
+    //     return false;
+    // } 
 
-    else if (inpnum.length!==10)
-    {
-        errornum.innerHTML = "*Please Enter 10 Digit Number "
-        errornum.style.color = "red"
-        return false;
+    // else if (inpnum.length!==10)
+    // {
+    //     errornum.innerHTML = "*Please Enter 10 Digit Number "
+    //     errornum.style.color = "red"
+    //     return false;
         
-    }
+    // }
 
-    else if(!(inpmail.includes('@') && inpmail.includes(".com")) )  {
+    // else if(!(inpmail.includes('@') && inpmail.includes(".com")) )  {
 
-        errormail.innerHTML="*Please Enter valid Email"
-        errormail.style.color="red"
-        return false;
+    //     errormail.innerHTML="*Please Enter valid Email"
+    //     errormail.style.color="red"
+    //     return false;
 
-    }
+    // }
 
-    else if(!(inppass.match([/1234567890/]) && inppass.match([/!@#$%^&*/]) && inppass.match([/a-z/]) && inppass.match([/A-Z/] )))
-    {
-        errorpass.innerHTML="*Enter A Strong Password";
-        errorpass.style.color="red"
-        return false;
-    }
+    // else if(!(inppass.match([/1234567890/]) && inppass.match([/!@#$%^&*/]) && inppass.match([/a-z/]) && inppass.match([/A-Z/] )))
+    // {
+    //     errorpass.innerHTML="*Enter A Strong Password";
+    //     errorpass.style.color="red"
+    //     return false;
+    // }
+
+    // let box=document.querySelector("btn")
+    // let box1=document.querySelector("#box")
+    // box1.addEventListener("click",()=>{
+    //     box1.style.backgroundColor="red"
+    // })
+
+    let inpbox=document.querySelector("#inpbox")
+
+    inpbox.addEventListener("input",()=>{
+        console.log(inpbox.value)
+    })
+
         
         
        
-}
+// }
---
+++ math.js
@@ -0,0 +1,13 @@
+console.log(Math.PI);
+
+console.log(Math.abs(-9));
+
+console.log(Math.floor(Math.random()*9000+1000));
+
+let  btn=()=>{
+    console.log(Math.floor(Math.random()*10));
+
+}
+
+
+
