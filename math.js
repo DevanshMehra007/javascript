da7c28eea3336e17c2a66a56872b1d2ccbcefea0
DevanshMehra007 <devanshkmehra10@gmail.com>
2025-02-09 22:44:23 +0530

    update

--- math.js
+++ math.js
@@ -5,7 +5,7 @@ console.log(Math.abs(-9));
 console.log(Math.floor(Math.random()*9000+1000));
 
 let  btn=()=>{
-    console.log(Math.floor(Math.random()*10));
+    console.log(Math.floor(Math.random()*6));
 
 }
 
