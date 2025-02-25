5f4a1792ac2fcb8606f8e1586e4e5c5b3e998e1f
DevanshMehra007 <devanshkmehra10@gmail.com>
2025-02-25 17:24:21 +0530

    update

---
+++ vanta.js
@@ -0,0 +1,13 @@
+VANTA.CELLS({
+    el: "#cells",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    color1: 0x7d2b2b,
+    color2: 0xff0000,
+    size: 1.80,
+    speed: 0.90
+  })
